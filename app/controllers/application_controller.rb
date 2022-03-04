287c6f1c82b2231fead63cf36c0173e5c0a71bcb
Henry Miller <mail.henrym+git@gmail.com>
2022-03-03 21:52:05 -0500

    commit tree 53be6df121051772386185eaee336e95080c49ea

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,32 @@
 class ApplicationController < Sinatra::Base
-
+  set :default_content_type, 'application/json'
   # add routes
+  get '/bakeries' do
+    # get all the bakeries from the database
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    # look up the bakery in the database using its ID
+    bakery = Bakery.find(params[:id])
+
+    bakery.to_json(include: :baked_goods)
+
+  end
+
+  get '/baked_goods/by_price' do
+    # get all the bakeries from the database
+    # baked_goods = BakedGood.by_price
+    baked_goods = BakedGood.all.order(price: :desc)
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do 
+    # baked_goods = BakedGood.by_price.first
+    baked_goods = BakedGood.all.order(price: :desc).first
+    baked_goods.to_json
+  end
+
 
 end
